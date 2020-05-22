b10e1a103483aed2ed921c619d79ce4730fa6a20
Rohan Pandey <32777448+KhoomeiK@users.noreply.github.com>
2020-05-21 21:16:37 -0700

    commented for clarity

--- comparer.py
+++ comparer.py
@@ -16,24 +16,24 @@ with conn:
 	c.execute('SELECT email, languages, frameworks FROM volunteers;')
 	volunteers = c.fetchall()
 
-	assignments = dict((vol[0], []) for vol in volunteers)
+	assignments = dict((vol[0], []) for vol in volunteers) # {volEmail: []}
 
 	for proj in projects:
 		print(proj[0], '-----------------------')
-		issues = get_issues(proj[0])
-		projTech = set(proj[1].split(',') + proj[2].split(','))
+		issues = get_issues(proj[0]) # get issues for github repo
+		projTech = set(proj[1].split(',') + proj[2].split(',')) # create set of langs & frameworks for proj
 
 		for vol in volunteers:
-			volTech = set(vol[1].split(',') + vol[2].split(','))
-			overlap = projTech.intersection(volTech)
+			volTech = set(vol[1].split(',') + vol[2].split(',')) # create set of langs & frameworks for vol
+			overlap = projTech.intersection(volTech) # overlap of proj and vol
 			if len(overlap) > 1:
-				print(vol[0], len(overlap))
+				print(vol[0], len(overlap)) # email, number of overlapping tech
 
-				ct = 0
+				ct = 0 # num of issues assigned
 				for issue in issues:
-					issueWords = set(issue['title'].split(' ')).union(set(issue['labels']))
-					if len(issueWords.intersection(keywords)) > 1 and ct < 3:
-						assignments[vol[0]].append(issue)
+					issueWords = set(issue['title'].split(' ')).union(set(issue['labels'])).intersection(keywords) # keywords in issue
+					if len(issueWords) > 1 and ct < 3:
+						assignments[vol[0]].append(issue) # assign issue to vol
 						ct += 1
 
 # to be passed to emailer						
