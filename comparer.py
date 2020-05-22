e71abaaa6b29f9b602776938c4050fb2709e1a06
Meet Vora <meetrajivvora@gmail.com>
2020-05-22 08:33:33 -0700

    added all code to compare method in comparer.py, and added code in main.py

--- comparer.py
+++ comparer.py
@@ -5,36 +5,37 @@ from repository_reader import get_issues
 languages = {'Python', 'Node', 'JavaScript', 'HTML', 'CSS', 'Java', 'C', 'C++', 'C#', 'Objective C', 'Go', 'Rust', 'Swift', 'Kotlin', 'Dart', 'Ruby', 'PHP', 'SQL', 'R', 'MATLAB', 'Assembly'}
 frameworks = {'Flask', 'Django', 'Express', 'React', 'Vue', 'Angular', 'React Native', 'Flutter', 'SpringBoot', '.NET', 'Rails', 'Laravel', 'WordPress', 'Unity', 'MySQL', 'Postgres', 'SQLite', 'Mongo', 'Firebase', 'Redis', 'TensorFlow', 'PyTorch', 'Keras', 'Scikit', 'Pandas'}
 keywords = languages.union(frameworks)
-
 database = 'database.db'
-conn = sqlite3.connect(database)
 
-with conn:
-	c = conn.cursor()
-	c.execute('SELECT repo, languages, frameworks FROM projects;')
-	projects = c.fetchall()
-	c.execute('SELECT email, languages, frameworks FROM volunteers;')
-	volunteers = c.fetchall()
+def compare():
+	conn = sqlite3.connect(database)
 
-	assignments = dict((vol[0], []) for vol in volunteers) # {volEmail: []}
+	with conn:
+		c = conn.cursor()
+		c.execute('SELECT repo, languages, frameworks FROM projects;')
+		projects = c.fetchall()
+		c.execute('SELECT email, languages, frameworks FROM volunteers;')
+		volunteers = c.fetchall()
 
-	for proj in projects:
-		print(proj[0], '-----------------------')
-		issues = get_issues(proj[0]) # get issues for github repo
-		projTech = set(proj[1].split(',') + proj[2].split(',')) # create set of langs & frameworks for proj
+		assignments = dict((vol[0], []) for vol in volunteers) # {volEmail: []}
 
-		for vol in volunteers:
-			volTech = set(vol[1].split(',') + vol[2].split(',')) # create set of langs & frameworks for vol
-			overlap = projTech.intersection(volTech) # overlap of proj and vol
-			if len(overlap) > 1:
-				print(vol[0], len(overlap)) # email, number of overlapping tech
+		for proj in projects:
+			print(proj[0], '-----------------------')
+			issues = get_issues(proj[0]) # get issues for github repo
+			projTech = set(proj[1].split(',') + proj[2].split(',')) # create set of langs & frameworks for proj
 
-				ct = 0 # num of issues assigned
-				for issue in issues:
-					issueWords = set(issue['title'].split(' ')).union(set(issue['labels'])).intersection(keywords) # keywords in issue
-					if len(issueWords) > 1 and ct < 3:
-						assignments[vol[0]].append(issue) # assign issue to vol
-						ct += 1
+			for vol in volunteers:
+				volTech = set(vol[1].split(',') + vol[2].split(',')) # create set of langs & frameworks for vol
+				overlap = projTech.intersection(volTech) # overlap of proj and vol
+				if len(overlap) > 1:
+					print(vol[0], len(overlap)) # email, number of overlapping tech
 
-# to be passed to emailer						
-print(assignments)
+					ct = 0 # num of issues assigned
+					for issue in issues:
+						issueWords = set(issue['title'].split(' ')).union(set(issue['labels'])).intersection(keywords) # keywords in issue
+						if len(issueWords) > 1 and ct < 3:
+							assignments[vol[0]].append(issue) # assign issue to vol
+							ct += 1
+
+# to be passed to emailer					
+# print(assignments)
--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-# can also send them a welcome/confirmation email for BitFix
-# need to make an email service for project managers as well, maybe about the number of people working on their project?
 
+######## Potential further features to add #############
+# 1. can also send them a welcome/confirmation email for BitFix
+# 2. need to make an email service for project managers as well, maybe about the number of people working on their project?
+# 3. can also send a sorry email to someone in the beginning of the day if we found no matches, 
+# 	or we can suggest another issue to them outside of their comfort zone and say this is available 
+# 	if they want to get out of their comfort zone and give it a try
+
+from emailer import run
 
 def main():
+	subject = "BitFix - Personalized Github Issues for the Day!"
+
 	# can also add the name of the person in the hello statement later
 	msg = """Hello!
 	Here are some issues from Github projects that require your attention!
-	
+
 	"""
-	print(msg)
+	# consolidate all the emails you need to send the email to
+	emails = []
+	run(subject, msg, emails)
+	# print(msg)
 
 if __name__ == '__main__':
     main()
