e53e0af4b917ff2c227f79a02d6b6e51b6db847d
Meet Vora <meetrajivvora@gmail.com>
2020-05-22 18:44:09 -0700

    changed compare() in comparer.py so that if an issue is invalid, does not save the link at all

--- comparer.py
+++ comparer.py
@@ -21,38 +21,42 @@ def compare():
 
 		for proj in projects:
 			# print(proj[0], '-----------------------')
-			issues = get_issues(proj[0])[::-1] # get issues for github repo
-			projTech = set(proj[1].split(',') + proj[2].split(',')) # create set of langs & frameworks for proj
+			temp = get_issues(proj[0]) # get issues for github repo
+			if temp is not None:
+				issues = temp[::-1]
+				projTech = set(proj[1].split(',') + proj[2].split(',')) # create set of langs & frameworks for proj
 
-			volRanks = []
-			for vol in volunteers:
-				volTech = set(vol[1].split(',') + vol[2].split(',')) # create set of langs & frameworks for vol
-				overlap = projTech.intersection(volTech) # overlap of proj and vol
-				if len(overlap) > 0 and len(assignments[vol[0]]) < 3:
-					volRanks.append((vol[0], len(overlap)))
-					# for issue in issues:
-					# 	issueWords = set(issue['title'].split(' ')).union(set(issue['labels'])).intersection(keywords) # keywords in issue
-					# 	if len(issueWords) > 1 and ct < 3:
-					# 		assignments[vol[0]].append(issue['link']) # assign issue to vol
-					# 		ct += 1
+				volRanks = []
+				for vol in volunteers:
+					volTech = set(vol[1].split(',') + vol[2].split(',')) # create set of langs & frameworks for vol
+					overlap = projTech.intersection(volTech) # overlap of proj and vol
+					if len(overlap) > 0 and len(assignments[vol[0]]) < 3:
+						volRanks.append((vol[0], len(overlap)))
+						# for issue in issues:
+						# 	issueWords = set(issue['title'].split(' ')).union(set(issue['labels'])).intersection(keywords) # keywords in issue
+						# 	if len(issueWords) > 1 and ct < 3:
+						# 		assignments[vol[0]].append(issue['link']) # assign issue to vol
+						# 		ct += 1
 
-			volRanks = sorted(volRanks, key=lambda x: x[1], reverse=True)
-			if len(volRanks) > 0:
-				if len(issues) > 0:
-					for issue in issues:
-						if len(assignments[volRanks[0][0]]) >= 3:
-							volRanks.pop(0)
-						if len(volRanks) == 0:
-							break
-						assignments[volRanks[0][0]].append(issue['link'])
-				else:
-					for vol in volRanks:
-						if len(assignments[vol[0]]) < 3:
-							assignments[vol[0]].append(proj[0])
-						issueWords = set(issue['title'].split(' ')).union(set(issue['labels'])).intersection(keywords) # keywords in issue
-						if len(issueWords) > 1 and ct < 3:
-							assignments[vol[0]].append(issue) # assign issue to vol
-							ct += 1
+				volRanks = sorted(volRanks, key=lambda x: x[1], reverse=True)
+				# print(volRanks)
+				if len(volRanks) > 0:
+					if len(issues) > 0:
+						for issue in issues:
+							if len(assignments[volRanks[0][0]]) >= 3:
+								volRanks.pop(0)
+							if len(volRanks) == 0:
+								break
+							assignments[volRanks[0][0]].append(issue['link'])
+					else:
+						for vol in volRanks:
+							print(proj[0])
+							if len(assignments[vol[0]]) < 3:
+								assignments[vol[0]].append(proj[0])
+							issueWords = set(issue['title'].split(' ')).union(set(issue['labels'])).intersection(keywords) # keywords in issue
+							if len(issueWords) > 1 and ct < 3:
+								assignments[vol[0]].append(issue) # assign issue to vol
+								ct += 1
 	return assignments
 
 # to be passed to emailer					
