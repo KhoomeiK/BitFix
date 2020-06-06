3a5060833cf1bea1308dfae545aa005acc39e4b4
Meet Vora <meetrajivvora@gmail.com>
2020-06-05 19:09:46 -0700

    got number of branches with beautifulsoup

--- devpostScraper/githubScraper.py
+++ devpostScraper/githubScraper.py
@@ -1,14 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+from urllib.request import urlopen
+
 import requests
 import sqlite3
 import json
 
+def scraper(url):
+	# opening up connection, grabbing the page
+	uClient = urlopen(url)
+	page_html = uClient.read()
+	uClient.close()
+
+	# html parsing
+	soup = BeautifulSoup(page_html, "lxml")
+	all_elements = soup.findAll('ul', class_='numbers-summary')[0].findAll('li')
+	num_branches = int(all_elements[1].span.text)
+	num_pull_reqs = all_elements
+
+	print(all_elements)
+
+url = "https://github.com/Meet-Vora/BitFix"
+scraper(url)
+
 # def get_issues(repo):
-user_name = 'Meet-Vora'
-repo_name = 'Slackbot'
-# gets the JSON-formatted data from the GitHub API
-response = requests.get("https://api.github.com/repos/" + user_name + "/" + repo_name + "/issues")
-print(response)
-print(response.links)
+# user_name = 'Meet-Vora'
+# repo_name = 'Slackbot'
+# # gets the JSON-formatted data from the GitHub API
+# response = requests.get("https://api.github.com/repos/" + user_name + "/" + repo_name + "/issues")
+# print(response)
+# print(response.links)
 	
 
 # get_issues('https://github.com/doc19org/medicam')
